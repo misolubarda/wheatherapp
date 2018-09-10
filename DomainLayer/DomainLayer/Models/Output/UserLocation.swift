3605ccc415f0b867cbe72305145700329b895020
Nikola Matijevic <nikolamatijevic101@gmail.com>
2018-09-10 15:19:07 +0200

    Introduce user location model

---
+++ DomainLayer/DomainLayer/Models/Output/UserLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UserLocation.swift
+//  DomainLayer
+//
+//  Created by Nikola Matijevic on 10/09/2018.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import Foundation
+
+public struct UserLocation {
+    let latitude: Double
+    let longitude: Double
+}
